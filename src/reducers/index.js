1d159dcfcfcb16dc30f2949320110bc723ae3e4f
agradilenko <44676263+agradilenko@users.noreply.github.com>
2020-07-02 13:55:15 +0300

    Initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,26 @@
+const initialState = [];
+let GLOBAL_ID = 1;
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      return [
+        ...state,
+        {
+          id: GLOBAL_ID++,
+          title: action.title,
+          completed: false
+        }
+      ];
+    case "TOGGLE_COMPLETION":
+      console.log('action', action);
+      return state.map(todo =>
+        todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
+      );
+    case "REMOVE_ITEM":
+      return state.filter(todo => todo.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+export default todos;
