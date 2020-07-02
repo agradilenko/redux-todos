ae25f607dc3a1f2986875624e8b70a439337c6bd
agradilenko <44676263+agradilenko@users.noreply.github.com>
2020-07-02 13:59:05 +0300

    Updates

---
+++ precache-manifest.459056c3cf78f5bf18dd590dbd533c49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61c41fb5ea687b024956",
+    "url": "/redux-todos/static/css/main.baa79d82.chunk.css"
+  },
+  {
+    "revision": "61c41fb5ea687b024956",
+    "url": "/redux-todos/static/js/main.61c41fb5.chunk.js"
+  },
+  {
+    "revision": "a722c451ac51003022bb",
+    "url": "/redux-todos/static/js/runtime~main.a722c451.js"
+  },
+  {
+    "revision": "b6553f42806e1084f56f",
+    "url": "/redux-todos/static/js/2.b6553f42.chunk.js"
+  },
+  {
+    "revision": "228af5be17d4d923d7295c17ec0446fa",
+    "url": "/redux-todos/index.html"
+  }
+];
